54f2ee919b78fcd522d921f466fcade7bd163ea7
CochiorDaniel <danielcochior24@yahoo.com>
2023-10-08 19:30:23 +0300

    Ex 1 done

---
+++ Lab1/ex1.py
@@ -0,0 +1,19 @@
+import math
+def gcd(n):
+    numbers = []
+
+    while n:
+        nr_dat = input()
+        nr = int(nr_dat)
+        numbers.append(nr)
+        n = n - 1
+
+    rezultat = numbers[0]
+    for nr in numbers[1:]:
+        rezultat = math.gcd(rezultat, nr)
+
+    print(rezultat)
+
+gcd(4)
+
+
